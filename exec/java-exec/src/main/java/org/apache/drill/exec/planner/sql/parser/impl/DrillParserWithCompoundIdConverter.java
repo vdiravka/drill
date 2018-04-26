afcf5627e5f06f6b517489c10c9d00cb4ba4b07a
Vitalii Diravka <vitalii.diravka@gmail.com>
2018-04-26 16:09:04 +0300

    DRILL-4682: test

--- exec/java-exec/src/main/java/org/apache/drill/exec/planner/sql/parser/impl/DrillParserWithCompoundIdConverter.java
+++ exec/java-exec/src/main/java/org/apache/drill/exec/planner/sql/parser/impl/DrillParserWithCompoundIdConverter.java
@@ -57,6 +57,8 @@ public class DrillParserWithCompoundIdConverter extends DrillParserImpl {
   @Override
   public SqlNode parseSqlStmtEof() throws Exception {
     SqlNode originalSqlNode = super.parseSqlStmtEof();
-    return originalSqlNode.accept(createConverter());
+    // TODO: When calcite will support recognizing map and array complex types
+    // need to return for using original SqlNode from calcite without converting
+    return originalSqlNode;
   }
 }
